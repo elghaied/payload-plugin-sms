62e1a093caec7d73d30d865929ad3eb2c9f49e29
elghaied <elghaied.eslam@gmail.com>
2026-05-26 03:46:57 +0200

    feat: add SMSValidationError and SMSProviderError

---
+++ src/errors.ts
@@ -0,0 +1,10 @@
+export class SMSValidationError extends Error {
+  override name = 'SMSValidationError'
+}
+
+export class SMSProviderError extends Error {
+  override name = 'SMSProviderError'
+  constructor(message: string, options?: { cause?: unknown }) {
+    super(message, options)
+  }
+}
